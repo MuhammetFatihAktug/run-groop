a90c488f89348b1f6f1788e417b2a1e4407b5bce
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-15 05:07:35 +0300

    Added some models

---
+++ src/main/java/org/example/rungroop/models/Club.java
@@ -0,0 +1,29 @@
+package org.example.rungroop.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "clubs")
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+
+}
