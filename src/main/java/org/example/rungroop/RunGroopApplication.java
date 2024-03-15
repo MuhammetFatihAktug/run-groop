dce77bc636a172372a031e5c9dc5d579f3225623
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-15 03:54:34 +0300

    first commit

---
+++ src/main/java/org/example/rungroop/RunGroopApplication.java
@@ -0,0 +1,13 @@
+package org.example.rungroop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunGroopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunGroopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rungroop/RunGroopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rungroop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunGroopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
