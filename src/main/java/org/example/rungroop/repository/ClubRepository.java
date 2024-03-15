94fa577a78beb8ee6734aa23eb6570a4226122b7
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-15 05:42:36 +0300

    N-Tier Architecture

---
+++ src/main/java/org/example/rungroop/repository/ClubRepository.java
@@ -0,0 +1,10 @@
+package org.example.rungroop.repository;
+
+import org.example.rungroop.models.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+    Optional<Club> findByTitle(String title);
+}
