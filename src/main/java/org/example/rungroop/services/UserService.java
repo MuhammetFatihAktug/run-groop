3639ff95c838582ce2439c0c95b3a7b93d45bad4
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-17 18:33:59 +0300

    login sections added

--- src/main/java/org/example/rungroop/services/UserService.java
+++ src/main/java/org/example/rungroop/services/UserService.java
@@ -1,7 +1,11 @@
 package org.example.rungroop.services;
 
 import org.example.rungroop.dto.RegistrationDto;
+import org.example.rungroop.models.UserEntity;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(String email);
+    UserEntity findByUsername(String username);
 }
--- src/main/java/org/example/rungroop/services/impl/UserServerImpl.java
+++ src/main/java/org/example/rungroop/services/impl/UserServerImpl.java
@@ -33,4 +33,14 @@ public class UserServerImpl implements UserService {
         user.setRoles(Arrays.asList(new Role[]{role}));
         userRepository.save(user);
     }
+
+    @Override
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
