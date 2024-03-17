0e2bf11ee4222a7d8d9989fa9df039ed9d9045fa
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-18 02:11:37 +0300

    Login and log out register mechanism added

---
+++ src/main/java/org/example/rungroop/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package org.example.rungroop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/login", "/register", "/clubs", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/clubs", true)
+                        .loginProcessingUrl("/login")
+                        .failureUrl("/login?error=true")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .permitAll());
+        return http.build();
+    }
+
+}
